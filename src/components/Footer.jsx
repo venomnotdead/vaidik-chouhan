6df4a1ecd432cd3d5f5e10f69b917c898fbb66fa
venomnotdead <vaidikchauhan333@gmail.com>
2024-03-12 16:19:42 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div>
+        <p>&copy; 2024 Vaidik Chouhan. All rights reserved.</p>
+        <p>Email: vaidikchauhan205@gmail.com</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
